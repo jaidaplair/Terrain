2355baa9dc25c97e266d9e4c9fbf7d2efa99176e
jaidaplair <90574364+jaidaplair@users.noreply.github.com>
2025-04-07 15:15:18 -0400

    changes

--- Assets/Assets.cs
+++ Assets/Assets.cs
@@ -2,3 +2,5 @@
 //https://sketchfab.com/3d-models/fantastic-castle-08a018e4e7b3486dbec7b0341be3aeb4
 //door
 //https://sketchfab.com/3d-models/medieval-door-038a9ea81ade4a56b6528e314b83140e
+//bridge
+//https://sketchfab.com/3d-models/low-poly-bridge-02217be5148946099483fb93a7e9ab0a
